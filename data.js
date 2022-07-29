27f4804e41a28351843e800cf86c48e1a93b9365
Husain Ahmmed <husainahmmed1b@gmail.com>
2022-07-29 14:46:02 +0600

    Initial commit

---
+++ data.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        img: "/images/1.jpg",
+        title: "Create a website using Html CSS and JavaScript",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        img: "/images/2.jpg",
+        title: "Complete portfolio website tutorial",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        img: "/images/3.jpg",
+        title: "Bootstrap 5 Complete tutorial",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        img: "/images/4.jpg",
+        title: "UIkit Complete tutorial",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        img: "/images/5.jpg",
+        title: "Tailwind CSS card design",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        img: "/images/6.jpg",
+        title: "Reactjs tutorial for beginner",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        img: "/images/7.jpg",
+        title: "Nextjs crash course",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        img: "/images/8.jpg",
+        title: "Create a website using Tailwind CSS",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+    {
+        img: "/images/9.jpg",
+        title: "Create an one page website using ReactJS",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    },
+]
